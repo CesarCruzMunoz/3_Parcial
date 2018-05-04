418ae9658ad0261af972c878aba60c67e6da565e
CesarCruzMunoz <35695493+CesarCruzMunoz@users.noreply.github.com>
2018-05-04 10:23:57 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -9,9 +9,9 @@ $table = "usuarios";
 $password = $_POST['pass1'];
 $correo = $_POST['correo'];
 
-$conexion = mysqli_connect($host, $user, $pass, $db);
+$conexion = mysqli_connect("localhost:3306", "root", "", "proyecto");
 
-$insert = "INSERT INTO".$table."(corre, password) VALUES('$correo', '$password')";
+$insert = "INSERT INTO usuarios (correo, password) VALUES('$correo', '$password')";
 
 $consulta = mysqli_query($conexion, $insert);
 
