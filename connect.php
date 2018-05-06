5653e8b33d34c80addcf93f7fef9476038b542e9
Jacob Elías <jacob1177a@gmail.com>
2018-05-06 00:42:03 -0500

    Revert "Saludos a todos. CÉSAR, NO SUBIR A ÉSTA BRANCH. JAJAJAJA."
    This reverts commit 96cc75c81a194bbb67b1c2d1eea8125d34adde9f.

--- connect.php
+++ connect.php
@@ -1,17 +1,12 @@
 <?php
-<<<<<<< HEAD
 
 $conexion = mysqli_connect("localhost", "root", "", "web");
 
-=======
-require 'conexion.php';
->>>>>>> 87400c9... Separación del JS del registro ya no lo muevas ¬¬#
 //Recibimos datos del html
 $correo = $_POST["correo"];
 $clave = $_POST["pass1"];
 $clave2 = $_POST["pass2"];
 
-<<<<<<< HEAD
 
 if ($clave==$clave2){
 	$insertar = "INSERT INTO usuarios (correo, password) VALUES ('$correo', '$clave')";
@@ -19,41 +14,12 @@ if ($clave==$clave2){
 	//guardar en BD
 	$resultado = mysqli_query($conexion, $insertar);
 
-=======
-$consulta = "SELECT * FROM usuarios WHERE correo='$correo'";
-
-if(!($result= $conexion->query($consulta))){
-	 $conexion-> close();
-	 die();
-}
-
-if($result->num_rows === 0){
-	$result-> free();//Liberamos lo que tiene la variable result
-	if ($clave==$clave2){
-		$insertar = "INSERT INTO usuarios (correo, password) VALUES ('$correo', '$clave')";
-		//guardar en BD
-		$resultado = mysqli_query($conexion, $insertar);
-		mysqli_close($conexion);
-		header('location: login.php');
-	}
-	else{
-		mysqli_close($conexion);
-		header('location: registro.php');
-	}
->>>>>>> 87400c9... Separación del JS del registro ya no lo muevas ¬¬#
 }
 else{
-	//Si no mandamos a registrar de nuevo
-	$result-> free();//Liberamos lo que tiene la variable result
-	$conexion-> close();
 	header('location: registro.php');
-	die();
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 87400c9... Separación del JS del registro ya no lo muevas ¬¬#
 //Cerrar conexion
+mysqli_close($conexion)
 
 ?>
