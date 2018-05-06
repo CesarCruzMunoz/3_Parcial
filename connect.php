87400c93ffb7304d855faa777f8c129e681f8011
Emi0706 <35703126+Emi0706@users.noreply.github.com>
2018-05-06 00:35:44 -0500

    Separación del JS del registro ya no lo muevas ¬¬#

--- connect.php
+++ connect.php
@@ -1,22 +1,40 @@
 <?php
-
 require 'conexion.php';
-
+//Recibimos datos del html
 $correo = $_POST["correo"];
 $clave = $_POST["pass1"];
 $clave2 = $_POST["pass2"];
 
-if ($clave==$clave2){
-	$insertar = "INSERT INTO usuarios (email, password) VALUES ('$correo', '$clave')";
-	//guardar en BD
-	$resultado = mysqli_query($conexion, $insertar);
+$consulta = "SELECT * FROM usuarios WHERE correo='$correo'";
 
-	header('location: login.php');
+if(!($result= $conexion->query($consulta))){
+	 $conexion-> close();
+	 die();
+}
+
+if($result->num_rows === 0){
+	$result-> free();//Liberamos lo que tiene la variable result
+	if ($clave==$clave2){
+		$insertar = "INSERT INTO usuarios (correo, password) VALUES ('$correo', '$clave')";
+		//guardar en BD
+		$resultado = mysqli_query($conexion, $insertar);
+		mysqli_close($conexion);
+		header('location: login.php');
+	}
+	else{
+		mysqli_close($conexion);
+		header('location: registro.php');
+	}
 }
 else{
+	//Si no mandamos a registrar de nuevo
+	$result-> free();//Liberamos lo que tiene la variable result
+	$conexion-> close();
 	header('location: registro.php');
+	die();
 }
+
+
 //Cerrar conexion
-mysqli_close($conexion)
 
 ?>
--- registro.php
+++ registro.php
@@ -5,7 +5,7 @@
 	<meta name="description" content=" Primer Proyecto de Programación Web">
 	<meta name="keywords" content="Universidad">
 	<title>Register</title>
-	<link rel="stylesheet" href="css/forum.css">
+	<link rel="stylesheet" type="text/css" href="css/forum.css">
 	<script type="text/javascript" src="js/validacion.js"></script>
 
 </head>
@@ -30,8 +30,8 @@
 
 	<h1>Formulario de Registro</h1>
 
-	<form action="connect.php" method="post" class="form-register"> 
-	
+	 <form action="connect.php" method="post" class="form-register"> 
+	<!--<form class="form-register"> !-->
 		<h2 class="form_titulo">
 			Registro de cuenta
 		</h2>
@@ -41,7 +41,7 @@
 			<input type="password" id="pass1" name="pass1" placeholder="Contraseña" class="input-48">
 			<input type="password" id="pass2" name="pass2" placeholder="Repite Contraseña" class="input-48">
 			<input type="submit" value="Crear" class="btn-enviar" onclick= "validar();">
-			<p class="form_link">¿Ya tienes una cuenta? <a href=login.php> Ingresa aqui</a></p>
+			<p class="form_link">¿Ya tienes una cuenta? <a href=login.html> Ingresa aqui</a></p>
 		</div>
 
 	</form>
