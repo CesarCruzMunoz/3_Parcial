488f55e1c797d009d889dae63a3108056688968f
CesarCruzMunoz <35695493+CesarCruzMunoz@users.noreply.github.com>
2018-05-06 00:32:19 -0500

    Registrar sin repetir

--- connect.php
+++ connect.php
@@ -1,21 +1,37 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "", "web");
-
+require 'conexion.php';
 //Recibimos datos del html
 $correo = $_POST["correo"];
 $clave = $_POST["pass1"];
 $clave2 = $_POST["pass2"];
 
-if ($clave==$clave2){
-	$insertar = "INSERT INTO usuarios (correo, password) VALUES ('$correo', '$clave')";
-	//guardar en BD
-	$resultado = mysqli_query($conexion, $insertar);
+$consulta = "SELECT * FROM usuarios WHERE correo='$correo'";
 
-	header('location: login.html');
+if(!($result= $conexion->query($consulta))){
+	 $conexion-> close();
+	 die();
+}
+
+if($result->num_rows === 0){
+	$result-> free();//Liberamos lo que tiene la variable result
+	if ($clave==$clave2){
+		$insertar = "INSERT INTO usuarios (correo, password) VALUES ('$correo', '$clave')";
+		//guardar en BD
+		$resultado = mysqli_query($conexion, $insertar);
+		mysqli_close($conexion);
+		header('location: login.php');
+	}
+	else{
+		mysqli_close($conexion);
+		header('location: registro.php');
+	}
 }
 else{
-	header('location: registro.html');
+	//Si no mandamos a registrar de nuevo
+	$result-> free();//Liberamos lo que tiene la variable result
+	mysqli_close($conexion);
+	header('location: registro.php');
+	die();
 }
-//Cerrar conexion
-mysqli_close($conexion)
+
 ?>
