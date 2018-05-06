6bd0a301ccff2430e7930d1b19a0be2e1c81d82f
CesarCruzMunoz <35695493+CesarCruzMunoz@users.noreply.github.com>
2018-05-05 21:17:26 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><!-- Para que el texto pueda leer símbolos como la ñ y los acentos-->
+	<meta name="description" content=" Primer Proyecto de Programación Web">
+	<meta name="keywords" content="Universidad">
+	<title>CEJ</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	
+</head>
+
+<body>
+
+	<br></br>
+
+	<header>
+		<div id="menu">
+			<ul> <!-- Agregar etiqueta li -->
+				<a href="index.php"> Index </a>
+				<a href="login.php""> Log In </a>
+				<a href="registro.php"> Register </a>
+			</ul>
+		</div>
+	</header>
+
+	<div id = "centro">
+		<h1> Proyecto Tercer Parcial</h1>
+		<br></br>
+		<br></br>
+
+		<img src="img\unicaribelogo.jpg", class= 'galeria2' />  <!-- Ingresar imagen con tamaño y al centro-->
+		
+		<h2>Integrantes</h2> 
+		<p class="informacion">  
+			Moises Emiliano Arellano Avila
+			<br>
+			Cesar Cruz Muñoz
+			<br>
+			Jacob Elias Torres
+		</p>
+		<br></br>
+		
+	</div>
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><!-- Para que el texto pueda leer símbolos como la ñ y los acentos-->
+	<meta name="description" content=" Primer Proyecto de Programación Web">
+	<meta name="keywords" content="Universidad">
+	<title>Register</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+</head>
+
+<body>
+
+
+	<br></br>
+
+	<header>
+		<div id="menu">
+			<ul> <!-- Agregar etiqueta li -->
+					<a href="index.php"> Index </a>
+					<a href="login.php""> Log In </a>
+					<a href="registro.php"> Register </a>
+			</ul>
+		</div>
+	</header>
+	
+	<br></br>
+	<br></br>
+
+	<h1>Log In</h1>
+
+	 <form action="validacion2.php" method="post" class="form-register"> 
+	<!--<form class="form-register"> !-->
+		<h2 class="form_titulo">
+			Inicio Sesion
+		</h2>
+
+		<div class="contenedor-inputs">
+			<input type="email" id="correo" name="correo" placeholder="correo" class="input-100">
+			<input type="password" id="password" name="password" placeholder="password" class="input-100">
+			<input type="submit" value="Acceder" class="btn-enviar">
+		</div>
+
+	</form>
+
+	<footer>
+		<p></p>
+	</footer>
+	
+</body>
+</html>
